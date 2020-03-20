018c64ffe24dcf0ea3969c08fd4dec0b33741d59
Jakub Figlak <jakub.figlak@gmail.com>
2020-03-20 21:48:48 +0100

    Complete react crash course

---
+++ react-crash-course/src/components/pages/About.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function About() {
+  return (
+    <>
+      <h1>About</h1>
+      <p>This is a TodoList app v1.0.0. It is part of a React Crash Course by Traversy Media</p>
+    </>
+  )
+}
